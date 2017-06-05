803bf0dd43226571150c1fd31a1a9d48803dfd44
varcyan <varcyan@qq.com>
2017-06-06 01:26:29 +0800

    again

--- src/components/content.vue
+++ src/components/content.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="wrap-scroll">
     <section class="content">
-      <custom-show :data="data.top_stories"></custom-show> 
+      <custom-show :data="data.top_stories"></custom-show>
       <custom-list :data="data.stories"></custom-list>
     </section>
   </section>
@@ -14,43 +14,44 @@ import BScroll from 'better-scroll'
 
 export default {
   name: 'content',
-  components:{
-    'custom-show':Show,
-    'custom-list':List
+  components: {
+    'custom-show': Show,
+    'custom-list': List
   },
-  data () {
+  data() {
     return {
       data: []
     }
   },
-  mounted () {
+  mounted() {
     this.$http.get('/news/latest')
-    .then((res)=>{
-      this.data = res.data
-      // console.log(JSON.stringify(data.data));
-      console.log(res.data);
-      // DOM 渲染完成后执行 better scroll
-      this.$nextTick(()=>{
-        console.log('更新');
-        let contScroll = document.getElementById('wrap-scroll')
-        let scroll = new BScroll(contScroll,{
-          snap: true
+      .then((res) => {
+        this.data = res.data
+        // console.log(JSON.stringify(data.data));
+        console.log(res.data);
+        // DOM 渲染完成后执行 better scroll
+        this.$nextTick(() => {
+          console.log('更新');
+          let contScroll = document.getElementById('wrap-scroll')
+          let scroll = new BScroll(contScroll, {
+            snap: true
+          })
         })
       })
-    })
-    
+
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-  #wrap-scroll {
-    width: 100%;
-    height: 100%;
-    background-color: #f3f3f3;
-  }
-  .content {
-    padding-top: 4.1rem;
-  }
+#wrap-scroll {
+  width: 100%;
+  height: 100%;
+  background-color: #f3f3f3;
+}
+
+.content {
+  padding-top: 4.1rem;
+}
 </style>
